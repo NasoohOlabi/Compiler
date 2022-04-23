d1420123e380987197689f831b349d3b767a600f
NasoohOlabi <nasooholabi@gmail.com>
2022-04-23 08:46:20 +0300

    clean up and organization

--- run.bat
+++
@@ -1,5 +0,0 @@
-"C:\Program Files (x86)\GnuWin32\bin\flex.exe" -oScanner.cpp scanner.l
-"C:\Program Files (x86)\GnuWin32\bin\bison.exe" -dtv -o Parser.cpp --define=Parser.h Parser.y
-g++ Parser.cpp Scanner.cpp Parser.h -o app
-
-app.exe

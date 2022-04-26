c797cb94540277f4e1ef01efda80695e1c1398b1
NasoohOlabi <nasooholabi@gmail.com>
2022-04-26 22:44:28 +0300

    added keywords and tested the program

--- Parser.h
+++ Parser.h
@@ -32,18 +32,78 @@
      NUMBER = 258,
      IDENT = 259,
      IF = 260,
-     ELSE = 261,
-     IFPREC = 262,
-     MINUS = 263
+     PROCEDURE = 261,
+     PROGRAM = 262,
+     VAR = 263,
+     INTEGER = 264,
+     REAL = 265,
+     TRUE = 266,
+     FALSE = 267,
+     LE = 268,
+     SE = 269,
+     EQ = 270,
+     NQ = 271,
+     NE = 272,
+     BOOLEAN = 273,
+     RETURN = 274,
+     DECREMENT = 275,
+     INCREMENT = 276,
+     NUL = 277,
+     FOR = 278,
+     WHILE = 279,
+     DOUBLE = 280,
+     FUNCTION = 281,
+     DO = 282,
+     BEG = 283,
+     END = 284,
+     ARRAY = 285,
+     DIV = 286,
+     OF = 287,
+     OR = 288,
+     AND = 289,
+     NOT = 290,
+     ELSE = 291,
+     IFPREC = 292,
+     MINUS = 293
    };
 #endif
 /* Tokens.  */
 #define NUMBER 258
 #define IDENT 259
 #define IF 260
-#define ELSE 261
-#define IFPREC 262
-#define MINUS 263
+#define PROCEDURE 261
+#define PROGRAM 262
+#define VAR 263
+#define INTEGER 264
+#define REAL 265
+#define TRUE 266
+#define FALSE 267
+#define LE 268
+#define SE 269
+#define EQ 270
+#define NQ 271
+#define NE 272
+#define BOOLEAN 273
+#define RETURN 274
+#define DECREMENT 275
+#define INCREMENT 276
+#define NUL 277
+#define FOR 278
+#define WHILE 279
+#define DOUBLE 280
+#define FUNCTION 281
+#define DO 282
+#define BEG 283
+#define END 284
+#define ARRAY 285
+#define DIV 286
+#define OF 287
+#define OR 288
+#define AND 289
+#define NOT 290
+#define ELSE 291
+#define IFPREC 292
+#define MINUS 293
 
 
 
--- Parser.y
+++ Parser.y
@@ -12,6 +12,36 @@
 %token NUMBER
 %token IDENT
 %token IF
+%token PROCEDURE
+%token PROGRAM
+%token VAR
+%token INTEGER
+%token REAL
+%token TRUE
+%token FALSE
+%token LE
+%token SE
+%token EQ
+%token NQ
+%token NE
+%token BOOLEAN
+%token RETURN
+%token DECREMENT
+%token INCREMENT
+%token NUL
+%token FOR
+%token WHILE
+%token DOUBLE
+%token FUNCTION
+%token DO
+%token BEG
+%token END
+%token ARRAY
+%token DIV
+%token OF
+%token OR
+%token AND
+%token NOT
 
 
 %nonassoc ELSE

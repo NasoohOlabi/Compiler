26d431e646522723daed8dc7d5ddef91103c31e2
NasoohOlabi <nasooholabi@gmail.com>
2022-04-28 21:55:11 +0300

    Removed files from the index (now ignored)

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/nasooh/Desktop/School_is_cool/Compilers/Compiler
-# Build directory: C:/Users/nasooh/Desktop/School_is_cool/Compilers/Compiler/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
